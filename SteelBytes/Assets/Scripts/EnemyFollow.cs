a14129db672e252d11bc429c8caf65034d834d5d
Jared Peterson <jared101@nmsu.edu>
2018-11-08 22:49:39 -0700

    added enemy to follow player, and camera stays on player

---
+++ SteelBytes/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed;
+    public float stoppingDistance;
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            if (target.position.x > transform.position.x)
+            {
+                //face right
+                transform.localScale = new Vector3(2, transform.localScale.y, transform.localScale.z);
+            }
+            else if(target.position.x < transform.position.x)
+            {
+                //face left
+                transform.localScale = new Vector3(-2, transform.localScale.y, transform.localScale.z);
+            }
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+	}
+
+    public void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
