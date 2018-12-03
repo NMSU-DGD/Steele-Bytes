3e57dd9923847928ec59f3084678327220bae06e
McGovern95 <mcgovernchristian@gmail.com>
2018-12-02 20:09:06 -0700

    got lasers semi-working, need collision

--- SteelBytes/Assets/Scripts/PlayerAttacks.cs
+++ SteelBytes/Assets/Scripts/PlayerAttacks.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerAttacks : MonoBehaviour {
 
     public int heat;
     Rigidbody2D player;
 
+     [Header("Laser pieces")]
+    public GameObject laserStart;
+    public GameObject laserMiddle;
+    public GameObject laserEnd;
+
+    private GameObject start;
+    private GameObject middle;
+    private GameObject end;
+
 	// Use this for initialization
 	void Start () {
+
         heat = 0;
         player = GetComponent<Rigidbody2D>();
+   
 	}
 
     private void FixedUpdate()
@@ -21,23 +34,86 @@ public class PlayerAttacks : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right);
+    if(Input.GetMouseButtonDown(1) && heat <100){
+		// Create the laser start from the prefab
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            heat += 10;
-            Debug.Log("Heat is " + heat);
-            if (hit.collider != null)
-            {
-                Debug.Log("I hit" + hit.collider.tag);
-            }
-        } else if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (heat > 50)
-                heat -= 50;
-            else
-                heat = 0;
-        }        
+			heat += 10;
+			Debug.Log("Heat is " + heat);       
+			    
+			if (start == null)
+			{
+			  start = Instantiate(laserStart) as GameObject;
+			  start.transform.parent = this.transform;
+			  start.transform.localPosition = Vector2.zero;
+			}
 
-	}
+			// Laser middle
+			if (middle == null)
+			{
+			  middle = Instantiate(laserMiddle) as GameObject;
+			  middle.transform.parent = this.transform;
+			  middle.transform.localPosition = Vector2.zero;
+			}
+
+			// Define an "infinite" size, not too big but enough to go off screen
+			float maxLaserSize = 20f;
+			float currentLaserSize = maxLaserSize;
+
+			// Raycast at the right as our sprite has been design for that
+			Vector2 laserDirection = this.transform.right;
+			RaycastHit2D hit = Physics2D.Raycast(this.transform.position, laserDirection, maxLaserSize);
+
+			if (hit.collider != null && hit.collider.gameObject.tag != "Player")
+			{
+			  // We touched something!
+
+			  // -- Get the laser length
+			  currentLaserSize = Vector2.Distance(hit.point, this.transform.position);
+
+			  // -- Create the end sprite
+			  if (end == null)
+			  {
+			    end = Instantiate(laserEnd) as GameObject;
+			    end.transform.parent = this.transform;
+			    end.transform.localPosition = Vector2.zero;
+			  }
+			}
+			else
+			{
+			  // Nothing hit
+			  // -- No more end
+			  if (end != null) Destroy(end);
+			}
+
+			// Place things
+			// -- Gather some data
+			float startSpriteWidth = start.GetComponent<Renderer>().bounds.size.x;
+			float endSpriteWidth = 0f;
+			if (end != null) endSpriteWidth = end.GetComponent<Renderer>().bounds.size.x;
+
+			// -- the middle is after start and, as it has a center pivot, have a size of half the laser (minus start and end)
+			middle.transform.localScale = new Vector3(currentLaserSize - startSpriteWidth, middle.transform.localScale.y, middle.transform.localScale.z);
+			middle.transform.localPosition = new Vector2((currentLaserSize/2f), 0f);
+
+			// End?
+			if (end != null)
+			{
+			  end.transform.localPosition = new Vector2(currentLaserSize, 0f);
+			} 
+  }//end button click
+    else if(Input.GetMouseButtonUp(1)){
+        DestroyImmediate(start);
+        DestroyImmediate(middle);
+        DestroyImmediate(end);
+    }
+
+    if (Input.GetKeyDown(KeyCode.E))
+    {
+        if (heat > 50)
+            heat -= 50;
+        else
+            heat = 0;
+    }  	
+
+  }
 }
